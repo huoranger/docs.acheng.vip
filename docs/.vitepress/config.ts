5d45e07b975c21280fb369055589f55489bbc51b
huoranger <huoranger@163.com>
2023-06-21 22:03:28 +0800

    修改配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,6 +10,7 @@ export default withMermaid(
     lang: metaData.lang,
     title: metaData.title,
     description: metaData.description,
+    ignoreDeadLinks: true,
 
     cleanUrls: true,
     lastUpdated: true, // 显示最后更新时间
